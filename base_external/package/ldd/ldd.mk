1a2dde2d1f6cd5c93e16b381b7376f7567a0f3fe
Mayuresh <mayuresh@Mayuresh.myguest.virtualbox.org>
2026-03-01 12:01:57 -0700

    fixed module not found issues

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -12,5 +12,9 @@ LDD_VERSION = main
 # This is critical: It tells Buildroot exactly which folders have the Makefiles
 LDD_MODULE_SUBDIRS = scull misc-modules
 
+define LDD_INSTALL_INIT_SYSV
+	$(INSTALL) -m 0755 $(LDD_PKGDIR)/S98lddmodules $(TARGET_DIR)/etc/init.d/S98lddmodules
+endef
+
 $(eval $(kernel-module))
 $(eval $(generic-package))
